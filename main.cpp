5d75cf4ca9eb885359b7b27d4c2c9d8a1323619d
charles <charles>
2011-07-20 02:24:42 +0000

    expand credits list

--- main.cpp
+++ main.cpp
@@ -10,12 +10,23 @@ int main( int argc, char **argv )
 {
 	KAboutData aboutData(
 			"meow", 0, ki18n( "Meow" ),
-			"1.0", ki18n("Cutest Music Player Ever"), KAboutData::License_BSD,
-			ki18n("(c) 2008-2011, Charles Samuels")
+			"1.0", ki18n("Cutest Music Player Ever"), KAboutData::License_GPL_V3,
+			ki18n("(c) 2008-2011, Charles Samuels. (c) 1994-2010 others")
 		);
 	
 	aboutData.addAuthor( ki18n("Charles Samuels"), ki18n("Developer"), "charles@kde.org" );
-	aboutData.addAuthor( ki18n("Allan Sandfeld Jensen"), ki18n("aKode"));
+	aboutData.addAuthor( ki18n("Allan Sandfeld Jensen"), ki18n("Most of Akode backend"));
+	aboutData.addAuthor( ki18n("Stefan Gehn, Charles Samuels"), ki18n("Portions of playback controller"));
+	aboutData.setBugAddress( "http://derkarl.org/meow");
+
+	aboutData.addCredit( ki18n("Josh Coalson"), ki18n("FLAC decoder"));
+	aboutData.addCredit( ki18n("the Xiph.Org Foundation"), ki18n("Vorbis decoder"));
+	aboutData.addCredit( ki18n("Underbit Technologies, Inc"), ki18n("mp3 decoder"));
+	aboutData.addCredit( ki18n("Buschmann/Klemm/Piecha/Wolf"), ki18n("Musepack decoder"));
+	aboutData.addCredit( ki18n("Peter Pawlowski"), ki18n("Musepack decoder"));
+	aboutData.addCredit( ki18n("Ross P. Johnson"), ki18n("Posix threads library for Windows"));
+	aboutData.addCredit( ki18n("The Public Domain"), ki18n("SQLite"));
+	aboutData.addCredit( ki18n("Kittens Everywhere"));
 	
 	KCmdLineArgs::init( argc, argv, &aboutData );
 	
