2aa100533ec2fed42fef73714f8e262b22ac3083
charles <charles>
2011-11-06 04:11:31 +0000

    do that bind mount

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-
 user="$1"
+
+
+mount --bind /var/stable-ia32/home /var/stable-ia64/home/
 
 i386 chroot /var/stable-ia32 su - $user -c 'cd ~/meow; mkdir -p build-chroot32; cd build-chroot32; export PATH=/home/charles/cmake-2.8.5-Linux-i386/bin/:$PATH; cmake .. -DCMAKE_INSTALL_PREFIX=/usr; make -j6 package; mv *.deb ..' &
 
