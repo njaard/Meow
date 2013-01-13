294c3a08ad23b2945157664c305324759f4a40e4
charles <charles>
2013-01-13 23:13:36 +0000

    change icon to "speaker"

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -102,7 +102,9 @@ Meow::MainWindow::MainWindow()
 	
 	d->scrobble = new Scrobble(this, d->player, d->collection);
 	setCentralWidget(owner);
-	
+
+	setWindowIcon(KIcon("speaker"));
+
 	d->tray = new KSystemTrayIcon("speaker", this);
 	d->tray->installEventFilter(this);
 	d->tray->show();
