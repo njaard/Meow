45b6070cc7f8088fe5dc273f26a220d54ebb688e
charles <charles>
2014-01-13 21:42:52 +0000

    all platform

--- releasetools/release-tools.sh
+++ releasetools/release-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#platforms="fedora-i386 fedora-x86_64 stable-i386 stable-x86_64 debian-unstable-win32-x86_64"
-platforms="debian-unstable-win32-x86_64"
+platforms="fedora-i386 fedora-x86_64 stable-i386 stable-x86_64 debian-unstable-win32-x86_64"
+#platforms="debian-unstable-win32-x86_64"
 version="$1"
 
 for i in $platforms;
