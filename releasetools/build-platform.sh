aa068a4d69dabd182b5702762fdf8a1e377ab1a0
charles <charles>
2014-01-13 03:16:42 +0000

    more build fixes

--- releasetools/build-platform.sh
+++ releasetools/build-platform.sh
@@ -11,16 +11,33 @@ then
 	meow_package=RPM
 fi
 
-mkdir build-$platform-kde
-cd build-$platform-kde
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DMEOW_PACKAGE=$meow_package
-make -j6 package
-mv *.$packagefmt ../../packages
-cd ..
+if [[ $platform == "debian-unstable-win32-x86_64" ]];
+then
+	mkdir build-win32
+	cd build-win32
+	cmake .. -DEXTRALIBS=/home/charles/dev/dlls -DCMAKE_TOOLCHAIN_FILE=../releasetools/Toolchain-mingw32.cmake
+	make -j4
 
-mkdir build-$platform-qt
-cd build-$platform-qt
-cmake .. -DMEOW_QT=1 -DCMAKE_INSTALL_PREFIX=/usr -DMEOW_PACKAGE=$meow_package
-make -j6 package
-mv *.$packagefmt ../../packages
+	mv meow.exe meow_$version.exe
+	i686-w64-mingw32-strip meow_$version.exe
+	zip -9 meow_$version.zip meow_$version.exe
+	upx --ultra-brute meow_$version.exe
 
+	mv meow_$version.exe meow_$version.zip ../../packages
+
+else
+	mkdir build-$platform-kde
+	cd build-$platform-kde
+	cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DMEOW_PACKAGE=$meow_package
+	make -j6 package
+	mv *.$packagefmt ../../packages
+	cd ..
+
+	mkdir build-$platform-qt
+	cd build-$platform-qt
+	cmake .. -DMEOW_QT=1 -DCMAKE_INSTALL_PREFIX=/usr -DMEOW_PACKAGE=$meow_package
+	make -j6 package
+	mv *.$packagefmt ../../packages
+
+
+fi
--- releasetools/release-tools.sh
+++ releasetools/release-tools.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-platforms="fedora-i386 fedora-x86_64 stable-i386 stable-x86_64"
+platforms="fedora-i386 fedora-x86_64 stable-i386 stable-x86_64 debian-unstable-win32-x86_64"
 
 for i in $platforms;
 do
@@ -8,6 +8,7 @@ do
 	umount /var/meow-builds/$i/proc
 	umount /var/meow-builds/$i/dev
 	mount --bind /var/meow-builds/home /var/meow-builds/$i/home/
+	mount --bind /home/charles/dev/dlls /var/meow-builds/home/charles/dlls
 	mount proc -t proc /var/meow-builds/$i/proc/
 	mount udev -t devtmpfs /var/meow-builds/$i/dev
 
@@ -15,6 +16,7 @@ do
 	arch=`echo $i | sed 's/^.*-//'`
 	package_fmt=deb
 	if [[ $i =~ fedora ]]; then package_fmt=rpm; fi
+	if [[ $i =~ win32 ]]; then package_fmt=exe; fi
 	$arch chroot /var/meow-builds/$i su - charles -c \
 		"cd ~/meow; bash releasetools/build-platform.sh $i $package_fmt 2> $i-build.log"
 done
